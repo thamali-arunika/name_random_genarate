4e72ee0d0ae4641fcb80f9261c99375acf5dd268
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-11-23 15:16:47 +0530

    title get center

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,6 +12,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title:const Text(
           'Group Creater',
           style: TextStyle(
