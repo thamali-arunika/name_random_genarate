390d57d7fb8d901120366dc630c6ec35ae72316f
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-11-23 15:00:50 +0530

    use appbar clour as pink

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Group Creater',
       theme: ThemeData(
-        
+        primarySwatch:Colors.pink,
       ),
       home: const MyHomePage(),
     );
